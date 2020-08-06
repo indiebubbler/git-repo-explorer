0efbe594f661a473a470023a24e3ec0f3715fd06
unknown <krazybubbler@gmail.com>
2020-08-06 14:44:04 +0200

    Updates

---
+++ precache-manifest.a033fc2c0b5cc1f2b1dbcd97a4461de8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf96760fcec1ab18a6594e7eb6eb55c",
+    "url": "/git-repo-explorer/index.html"
+  },
+  {
+    "revision": "de3f91e5137301049858",
+    "url": "/git-repo-explorer/static/css/2.1087be86.chunk.css"
+  },
+  {
+    "revision": "64f75a040c9a80bd9687",
+    "url": "/git-repo-explorer/static/css/main.6d2d444d.chunk.css"
+  },
+  {
+    "revision": "de3f91e5137301049858",
+    "url": "/git-repo-explorer/static/js/2.f5edddd3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/git-repo-explorer/static/js/2.f5edddd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f75a040c9a80bd9687",
+    "url": "/git-repo-explorer/static/js/main.3d3cfe17.chunk.js"
+  },
+  {
+    "revision": "9200e82ca1a94cdf1cc7",
+    "url": "/git-repo-explorer/static/js/runtime-main.751107ac.js"
+  }
+]);
